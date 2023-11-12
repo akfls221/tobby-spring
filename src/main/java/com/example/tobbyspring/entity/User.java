0b8b827103d5cb6bd1a236e5b7a012ae40da3e71
엄태권 <akfls221@gmail.com>
2023-11-13 04:10:27 +0900

    feat : User 도메인 추가

---
+++ src/main/java/com/example/tobbyspring/entity/User.java
@@ -0,0 +1,11 @@
+package com.example.tobbyspring.entity;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+
+    private String id;
+    private String name;
+    private String password;
+}
