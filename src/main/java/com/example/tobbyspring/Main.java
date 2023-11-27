43b053df83013e79e3c1c92837863a0219f17195
엄태권 <akfls221@gmail.com>
2023-11-28 02:38:29 +0900

    feat : 테스트를 Main 추가

---
+++ src/main/java/com/example/tobbyspring/Main.java
@@ -0,0 +1,27 @@
+package com.example.tobbyspring;
+
+import com.example.tobbyspring.basic.UserDao;
+import com.example.tobbyspring.entity.User;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("owen");
+        user.setName("엄태권");
+        user.setPassword("password");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getPassword() + "조회 성공");
+
+    }
+}
