2639a17e1a002f2618b4d4461e57568ef6f001f8
엄태권 <akfls221@gmail.com>
2023-11-28 02:38:11 +0900

    feat : 관심사항을 분리한 UserDao 추가

---
+++ src/main/java/com/example/tobbyspring/basic/UserDao.java
@@ -0,0 +1,51 @@
+package com.example.tobbyspring.basic;
+
+import com.example.tobbyspring.entity.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobby", "root", "dobbyMaster");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobby", "root", "dobbyMaster");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User findUser = new User();
+        findUser.setId(resultSet.getString("id"));
+        findUser.setName(resultSet.getString("name"));
+        findUser.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return findUser;
+    }
+}
