2e2c300d02264e67d26c92e317014b537262416f
엄태권 <akfls221@gmail.com>
2023-12-05 02:49:43 +0900

    feat : 관심사항 클래스 분리 추가

---
+++ src/main/java/com/example/tobbyspring/basic/step4/SimpleConnectionMaker.java
@@ -0,0 +1,13 @@
+package com.example.tobbyspring.basic.step4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/tobby", "root", "dobbyMaster");
+    }
+}
---
+++ src/main/java/com/example/tobbyspring/basic/step4/UserDao_3.java
@@ -0,0 +1,53 @@
+package com.example.tobbyspring.basic.step4;
+
+import com.example.tobbyspring.entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao_3 {
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao_3() {
+        this.simpleConnectionMaker = new SimpleConnectionMaker();
+    }
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = simpleConnectionMaker.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = simpleConnectionMaker.getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id = ?");
+
+        preparedStatement.setString(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User findUser = new User();
+        findUser.setId(resultSet.getString("id"));
+        findUser.setName(resultSet.getString("name"));
+        findUser.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return findUser;
+    }
+}
