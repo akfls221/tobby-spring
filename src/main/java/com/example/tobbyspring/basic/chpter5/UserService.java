a7f111a021e43d7a8f3e11a49d2407a5d6252a2d
엄태권 <akfls221@gmail.com>
2024-01-03 02:58:47 +0900

    feat : Mock 메일을 통한 메일 발송 기능 추가

--- src/main/java/com/example/tobbyspring/basic/chpter5/UserService.java
+++ src/main/java/com/example/tobbyspring/basic/chpter5/UserService.java
@@ -4,6 +4,9 @@ import com.example.tobbyspring.basic.chpter5.step1.UserDao;
 import com.example.tobbyspring.entity.Level;
 import com.example.tobbyspring.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -17,7 +20,12 @@ public class UserService {
 
     private final UserDao userDao;
     private final PlatformTransactionManager platformTransactionManager;
+    private MailSender mailSender;
 
+    @Autowired
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void upgradeLevels() {
         TransactionStatus transaction = this.platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
@@ -28,6 +36,7 @@ public class UserService {
                 if (canUpgradeLevel(user)) {
                     user.upgradeLevel();
                     userDao.update(user);
+                    sendUpgradeEmail(user);
                 }
             }
 
@@ -36,6 +45,16 @@ public class UserService {
             this.platformTransactionManager.rollback(transaction);
             throw e;
         }
+    }
+
+    private void sendUpgradeEmail(User user) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getName());
+        message.setFrom("test@spring.com");
+        message.setSubject("Upgrade 안내");
+        message.setText("사용자님의 등급이 " + user.getLevel() + "로 상승했습니다.");
+
+        this.mailSender.send(message);
     }
 
     private boolean canUpgradeLevel(User user) {
