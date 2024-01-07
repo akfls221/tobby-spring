cd1ddf342edf112599786a1388573a11c31018f7
엄태권 <akfls221@gmail.com>
2024-01-07 19:34:17 +0900

    fix : dynamic 프록시 확장

--- src/main/java/com/example/tobbyspring/basic/chapter6/dynamicproxy/UppercaseHandler.java
+++ src/main/java/com/example/tobbyspring/basic/chapter6/dynamicproxy/UppercaseHandler.java
@@ -1,22 +1,26 @@
 package com.example.tobbyspring.basic.chapter6.dynamicproxy;
 
-import com.example.tobbyspring.basic.chapter6.Hello;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
 
-    private Hello target;
+    private Object target;
 
-    public UppercaseHandler(Hello target) {
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     //타킷의 meothod를 호출함
     @Override
-    public String invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String result = (String) method.invoke(target, args);
-        return result.toUpperCase();
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = method.invoke(target, args);
+
+        if (result instanceof String stringTypeResult) {
+            return stringTypeResult.toUpperCase();
+        }
+
+        return result;
+
     }
 }
