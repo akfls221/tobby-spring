141adba4876528bb42fe7536a0c5d403fe340322
엄태권 <akfls221@gmail.com>
2024-01-10 01:45:22 +0900

    feat : 클래스 필터가 포함된 포인트컷 생성

---
+++ src/main/java/com/example/tobbyspring/basic/chapter6/pointcut/NameMatchClassMethodPointcut.java
@@ -0,0 +1,26 @@
+package com.example.tobbyspring.basic.chapter6.pointcut;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
+
+    public void setMappedClassName(String mappedClassName) {
+        this.setClassFilter(new SimpleClassFilter(mappedClassName));
+    }
+
+    private class SimpleClassFilter implements ClassFilter {
+
+        private String mappedName;
+
+        public SimpleClassFilter(String mappedName) {
+            this.mappedName = mappedName;
+        }
+
+        @Override
+        public boolean matches(Class<?> clazz) {
+            return PatternMatchUtils.simpleMatch(mappedName, clazz.getSimpleName());
+        }
+    }
+}
