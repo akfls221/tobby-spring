b1ba9306e28605c31b5d412635e73e3da01fb5d1
엄태권 <akfls221@gmail.com>
2024-01-11 01:53:26 +0900

    feat : 테스트용 클래스 생성

---
+++ src/main/java/com/example/tobbyspring/basic/chapter6/pointcut/pratice/Bean.java
@@ -0,0 +1,8 @@
+package com.example.tobbyspring.basic.chapter6.pointcut.pratice;
+
+public class Bean {
+
+    public void method() throws RuntimeException {
+
+    }
+}
---
+++ src/main/java/com/example/tobbyspring/basic/chapter6/pointcut/pratice/TargetInterface.java
@@ -0,0 +1,12 @@
+package com.example.tobbyspring.basic.chapter6.pointcut.pratice;
+
+public interface TargetInterface {
+
+    void hello();
+
+    void hello(String text);
+
+    int minus(int a, int b) throws RuntimeException;
+
+    int plus(int a, int b);
+}
